7d5ee83406b83d23d27f48ec05fa136a3a65196d
“amcdaid106” <aoife.mcdaid@gmail.com>
2015-08-24 22:37:35 +0200

    add new item to list

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,15 @@
 // This stuff will eventually come from the server
-var allItemsCollection = new Backbone.Collection([
+var ItemModel = Backbone.Model.extend({
+    defaults: {
+        isDone: false
+    }
+});
+
+var ItemsCollection = Backbone.Collection.extend({
+    model: ItemModel
+});
+
+var allItemsCollection = new ItemsCollection([
     { id: 0, item: "Walk the dog", isDone: false },
     { id: 1, item: "Schedule doctor's appointment", isDone: false },
     { id: 2, item: "Pee", isDone: false },
@@ -18,10 +28,27 @@ var ItemView = Backbone.View.extend({
 });
 
 var $items = $('.items');
-allItemsCollection.forEach(function(itemModel) {
+
+function createItemView(itemModel) {
     var itemView = new ItemView({
         model: itemModel
     });
     $items.append(itemView.$el);
     itemView.render();
+}
+
+allItemsCollection.forEach(createItemView);
+
+allItemsCollection.on('add', createItemView);
+
+var $textarea = $('textarea');
+$textarea.focus();
+$textarea.on('keyup', function(e) {
+    if (e.which === 13) {
+        var newItem = $textarea.val().trim();
+        allItemsCollection.add({
+            item: newItem
+        });
+        $textarea.val('');
+    }
 });
