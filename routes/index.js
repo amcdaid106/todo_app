67722ceee7ee3e5ff4a7b5e682e1e014cc20b8a5
Chris Lyons <lyons.chr@gmail.com>
2015-08-30 17:52:28 +0200

    basic in-memory API

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,66 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Your To Do List' });
 });
 
+
+// In-memory store
+
+var id = -1;
+var items = {};
+
+router.get('/items', function(req, res) {
+    var itemsArr = Object.keys(items).map(function(key) {
+        return items[key];
+    });
+    res.json(itemsArr);
+});
+
+router.post('/items', function(req, res) {
+    var item = req.body;
+    id++;
+    item.id = id;
+    items[id] = item;
+    res.json(item);
+});
+
+router.get('/items/:id', function(req, res) {
+    var id = req.params.id;
+    var item = items[id];
+
+    if (item) {
+        res.json(item);
+    } else {
+        res.status(500).send({ error: 'Couldn\'t find item' });
+    }
+});
+
+router.put('/items/:id', function(req, res) {
+    var id = req.params.id;
+    var item = items[id];
+    var body = req.body;
+    var keys = ['item', 'isDone'];
+
+    if (item) {
+        keys.forEach(function(key) {
+            if (body[key] !== 'undefined') {
+                item[key] = body[key];
+            }
+        });
+        res.json(item);
+    } else {
+        res.status(500).send({ error: 'Couldn\'t find item' });
+    }
+});
+
+router.delete('/items/:id', function(req, res) {
+    var id = req.params.id;
+    var item = items[id];
+
+    if (!item) {
+        return res.status(500).send({ error: 'Couldn\'t find item' });
+    }
+
+    delete items[id];
+    res.status(200).send({ success: true });
+});
+
 module.exports = router;
